fc5db876efd743aede229f5179d69b2776f1b001
Alice Bardon <abardon.catineau@gmail.com>
2022-12-22 21:51:16 +0100

    valide letters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,19 @@
 class GamesController < ApplicationController
+
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def score
+    @letters = params[:letters]
+    @word = params[:word]
+    @word_letters = @word.split("")
+  end
+end
+
+
+private
+
+def included?
+
 end
